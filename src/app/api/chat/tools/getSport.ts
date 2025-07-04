77536f0e168d4d15a1546bd4cca0a2dbcb34df13
ahamdjin <ahamdjin34@gmail.com>
2025-07-04 09:36:00 +0500

    Update getSport.ts

--- src/app/api/chat/tools/getSport.ts
+++ src/app/api/chat/tools/getSport.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const getSports = tool({
   description:
-    "This tool returns a playful gallery prompt of my best gaming moments—clutches, fails, and checkmates with friends. This tools can be used when the user asks about my gaming highlights, favorite games, or memorable moments in gaming. Or anything related to sports for exacmple tell me about your sports.",
+    "This tool returns a playful gallery prompt of my best gaming moments—clutches, fails, and checkmates with friends. This tools can be used when the user asks about my gaming highlights, favorite games, or memorable moments in gaming. Or anything related to sports for exacmple tell me about your sports Or what do i do for Fun? Or what are my hobbies?",
   parameters: z.object({}),
   execute: async () => {
     return "Here are my top gaming highlights: headshots in Free Fire, epic blunders in Chess, and those ‘did-that-really-just-happen?’ squad moments. Ready to relive the fun?";
