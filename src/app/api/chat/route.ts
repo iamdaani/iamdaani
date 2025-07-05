99821953860bae674aa77f5ccf9f85fea12ba6f5
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2025-07-05 18:31:51 +0500

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,6 +10,7 @@ import { getProjects } from './tools/getProjects';
 import { getResume } from './tools/getResume';
 import { getSkills } from './tools/getSkills';
 import { getSports } from './tools/getSport';
+import { getExperience } from './tools/getExperience';
 
 export const maxDuration = 30;
 
@@ -40,6 +41,7 @@ export async function POST(req: Request) {
       getSports,
       getCrazy,
       getInternship,
+      getExperience,
     };
 
     // Kick off streaming with Groq + mistral-saba-24b
