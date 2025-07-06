ab700d4df7175ee56caa78486c333f5b31340384
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2025-07-06 23:44:40 +0500

    Update getProjects.ts

--- src/app/api/chat/tools/getProjects.ts
+++ src/app/api/chat/tools/getProjects.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 
 export const getProjects = tool({
   description:
-    "This tool will show a list of all projects made by Ahmad",
+    "This tool will show a list of all projects made by Ahmad and answer question anything related to projects",
   parameters: z.object({}),
   execute: async () => {
     return "Here are all the projects made by Ahmad (above)! Don't hesitate to ask me more about them! (don't ";
