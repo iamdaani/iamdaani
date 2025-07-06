b7a32c41737a36f5dc9396ee19bde550880b8824
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2025-07-07 01:20:28 +0500

    Update apple-cards-carousel.tsx

--- src/components/projects/apple-cards-carousel.tsx
+++ src/components/projects/apple-cards-carousel.tsx
@@ -133,15 +133,15 @@ export const Carousel = ({
                   opacity: 0,
                   y: 20,
                 }}
-                animate={{
+                whileInView={{
                   opacity: 1,
                   y: 0,
-                  
+                  viewport={{ once: true }}
                   transition: {
                     duration: 0.5,
                     delay: 0.2 * index,
                     ease: 'easeOut',
-                    viewport={{ once: true }}
+                    once: true,
                   },
                 }}
                 key={'card' + index}
