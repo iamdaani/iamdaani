47900249ba1dd2ad536406ca8e6e3a49f6dcaaf6
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2025-07-07 22:08:10 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,7 @@ export default function Home() {
           Hey, I'm Ahmad 👋
         </h2>
         <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
-          AI Portfolio
+          My Portfolio
         </h1>
       </motion.div>
 
